20c78b39ee8162a4f505dc4a2f16d7356b1a6714
Michael Nachbaur <mike@nachbaur.com>
2009-06-08 17:34:18 -0700

    Improved rename script to add test mode, and custom shortname support

--- iphone/rename-phonegap.sh
+++ iphone/rename-phonegap.sh
@@ -5,23 +5,27 @@ set -e
 usage()
 {
     [ -n "$1" ] && echo $1 >&2
-    echo "Usage: $0 [-r] [-v] [-g] -n <name>" >&2
-    echo "  -n  Specify the name of the new project" >&2
+    echo "Usage: $0 [-t] [-v] [-r] [-g] [-s <name>] -n <name>" >&2
+    echo "  -n  The full name of the new project" >&2
+    echo "  -s  The short name of the project; default: camel-cases the full name" >&2
     echo "  -r  Rename all instances of PhoneGap; use with care, as" >&2
     echo "      this will make it very difficult to merge in future updates" >&2
+    echo "  -t  Test only; don't actually make any changes" >&2
     echo "  -v  Enable verbose mode" >&2
     echo "  -g  Use git to rename files" >&2
     exit 1
 }
 
-renameall= verbose= usegit= appname=
-while getopts :v:n:r:g ch
+appname= shortname= renameall= verbose= usegit= testonly=
+while getopts s:n:tvrg ch
 do
     case "$ch" in
     n) appname="$OPTARG";;
+    s) shortname="$OPTARG";;
     r) renameall=TRUE;;
     g) usegit=TRUE;;
     v) verbose=TRUE;;
+    t) testonly=TRUE;;
     *) usage;;
     esac
 done
@@ -29,11 +33,13 @@ shift $(($OPTIND - 1))
 
 cd iphone
 
+TEST=
 CMD=
 [ -z "$appname" ] && usage
 [ -n "$usegit" ] && CMD=git
+[ -n "$testonly" ] && TEST=echo
 
-shortname=`echo $appname | sed -E -e 's/[^a-zA-Z0-9]//g'`
+[ -z "$shortname" ] && shortname=`echo $appname | sed -E -e 's/[^a-zA-Z0-9]//g'`
 [ -n "$verbose" ] && echo "Long name:  $appname"
 [ -n "$verbose" ] && echo "Short name: $shortname"
 
@@ -42,24 +48,24 @@ shortname=`echo $appname | sed -E -e 's/[^a-zA-Z0-9]//g'`
 [ -n "$verbose" ] && set -x
 
 xcodeproj=PhoneGap.xcodeproj/project.pbxproj
-$CMD mv PhoneGap.plist $shortname.plist
-sed -E -i '' -e "s/PhoneGap.plist/$shortname.plist/g"  $xcodeproj
-sed -E -i '' -e "s/PhoneGap.app/$shortname.app/g" $xcodeproj
-sed -E -i '' -e "s/name = PhoneGap/name = \"$appname\"/g" $xcodeproj
-sed -E -i '' -e "s/PRODUCT_NAME = PhoneGap/PRODUCT_NAME = $shortname/g" $xcodeproj
-sed -E -i '' -e "s/productName = PhoneGap/productName = $shortname/g" $xcodeproj
-sed -E -i '' -e "s/PhoneGap Demo/$appname/g" Info.plist
-sed -E -i '' -e "s/PhoneGap/$shortname/g" Info.plist
+$TEST $CMD mv PhoneGap.plist $shortname.plist
+$TEST sed -E -i '' -e "s/PhoneGap.plist/$shortname.plist/g"  $xcodeproj
+$TEST sed -E -i '' -e "s/PhoneGap.app/$shortname.app/g" $xcodeproj
+$TEST sed -E -i '' -e "s/name = PhoneGap/name = \"$appname\"/g" $xcodeproj
+$TEST sed -E -i '' -e "s/PRODUCT_NAME = PhoneGap/PRODUCT_NAME = $shortname/g" $xcodeproj
+$TEST sed -E -i '' -e "s/productName = PhoneGap/productName = $shortname/g" $xcodeproj
+$TEST sed -E -i '' -e "s/PhoneGap Demo/$appname/g" Info.plist
+$TEST sed -E -i '' -e "s/PhoneGap/$shortname/g" Info.plist
 
 if [ -n "$renameall" ]; then
-    sed -E -i '' -e "s/PhoneGap/$shortname/g" $xcodeproj
+    $TEST sed -E -i '' -e "s/PhoneGap/$shortname/g" $xcodeproj
     for file in `find . -type f -name '*.xib' -o -name '*.h' -o -name '*.m' -o -name '*.pch' | grep -v Frameworks`; do
-        sed -E -i '' -e "s/PhoneGap/$shortname/g" $file
+        $TEST sed -E -i '' -e "s/PhoneGap/$shortname/g" $file
         newfilename=`echo $file | sed -e s/PhoneGap/$shortname/`
         if [ $newfilename != $file ]; then
-            $CMD mv "$file" "$newfilename"
+            $TEST $CMD mv "$file" "$newfilename"
         fi
     done
 fi
 
-$CMD mv PhoneGap.xcodeproj $shortname.xcodeproj
+$TEST $CMD mv PhoneGap.xcodeproj $shortname.xcodeproj
