b13a9246ff325423fa690cb45541648349f2b541
robb1e <robert.clutton@gmail.com>
2009-06-16 15:01:55 -0700

    Use the AVFoundation package to use in application HTTP streaming audio

--- iphone/Classes/Sound.m
+++ iphone/Classes/Sound.m
@@ -25,6 +25,20 @@
                                          inDirectory:directoryStr];
     if (filePath == nil) {
         NSLog(@"Can't find filename %@ in the app bundle", [arguments objectAtIndex:0]);
+	if ([[arguments objectAtIndex:0] hasPrefix:@"http"]){
+	
+	    if (player != nil)
+		[player stop];
+	    NSURL *sampleUrl = [NSURL URLWithString:[arguments objectAtIndex:0]];
+            NSData *sampleAudio = [NSData dataWithContentsOfURL:sampleUrl];
+	    NSError *err;
+            player = [[ AVAudioPlayer alloc ] initWithData:sampleAudio error:&err];
+	    if (err)
+		NSLog(@"Failed to initialize AVAudioPlayer: %@\n", err);
+	    player.delegate = self;
+            [ player prepareToPlay ];
+            [ player play ];
+	}
         return;
     }
     SystemSoundID soundID;
